a05fe77f1f7cb0ee5e494c60d542b57b5d8de395
Morning <morning@mstars-lab3-15s-iMac.local>
2023-01-24 12:39:32 +0800

    fd

--- src/components/admin/Admin.js
+++ src/components/admin/Admin.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
+import Dashboard from "./Dashboard";
 
 export default function Admin() {
   const [news, setNews] = useState([]);
@@ -65,80 +66,6 @@ export default function Admin() {
           </div>
           <div className="col-md-9">
             <Outlet />
-            <div>
-              <div className="container-fluid">
-                <div className="row">
-                  <div className="col-md-12 px-4">
-                    <div className="row my-4">
-                      <div className="col-md-4">
-                        <div className="card py-2 px-4">
-                          <h5 class="card-title text-warning">Users</h5>
-                          <div className="d-flex justify-content-between align-items-center">
-                            <div className="fs-1">
-                              <i
-                                className="bi bi-people"
-                                style={{ fontSize: "40px" }}
-                              ></i>
-                            </div>
-                            <h2 className="text-primary">4500</h2>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="col-md-4">
-                        <div className="card py-2 px-4">
-                          <h5 class="card-title text-warning">News</h5>
-                          <div className="d-flex justify-content-between align-items-center">
-                            <div className="fs-1">
-                              <i
-                                className="bi bi-newspaper"
-                                style={{ fontSize: "40px" }}
-                              ></i>
-                            </div>
-                            <h2 className="text-primary">2300</h2>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="col-md-4">
-                        <div className="card py-2 px-4">
-                          <h5 class="card-title text-warning">Access</h5>
-                          <div className="d-flex justify-content-between align-items-center">
-                            <div className="fs-1">
-                              <i
-                                className="bi bi-reception-4"
-                                style={{ fontSize: "40px" }}
-                              ></i>
-                            </div>
-                            <h2 className="text-primary">6400</h2>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="col-md-12">
-                    </div>
-                    <div className="table-responsive">
-                      <table className="table">
-                        <thead>
-                          <th>1</th>
-                          <th>1</th>
-                          <th>1</th>
-                        </thead>
-                        <tbody>
-                          {news.map((e) => {
-                            return (
-                              <tr>
-                                <td>a</td>
-                                <td>b</td>
-                                <td>b</td>
-                              </tr>
-                            );
-                          })}
-                        </tbody>
-                      </table>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
           </div>
         </div>
       </div>
